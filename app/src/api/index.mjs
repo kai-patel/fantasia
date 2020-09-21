226f5c0dc32a3507abd6d1d8342a800dbdffe5a3
Kai Patel <00kaipatel@gmail.com>
2020-09-21 15:20:53 +0100

    Added basic API query skeleton

---
+++ app/src/api/index.mjs
@@ -0,0 +1,56 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let api_key = process.env.API_KEY;
+let base = process.env.DEMO
+    ? "https://api-football.com/demo/v2"
+    : "https://v2.api-football.com/";
+
+if (process.env.DEMO) {
+    console.log("!!! --- USING DEMO API --- !!!");
+} else {
+    console.log("!!!!!! ------ USING PRODUCTION API ------ !!!!!!");
+}
+
+const instance = axios.create({
+    baseURL: base,
+    headers: { "X-RapidAPI-Key": api_key },
+});
+
+const queryAPI = async (endpoint) => {
+    try {
+        let result = await instance.get(`${endpoint}`);
+        //console.log(`Fetching data from endpoint: ${endpoint}`);
+        //console.log(result);
+        return result.data;
+    } catch (e) {
+        console.error(e.message);
+        return false;
+    }
+    return false;
+};
+
+// GET : https://v2.api-football.com/leagues/type/league/England/{year}
+const getPremierLeagueID = async (year) => {
+    try {
+        let result = await queryAPI(`leagues/type/league/England/${year}`);
+        if (result != false) {
+            let leagues = result["api"]["leagues"];
+            leagues.forEach((league) => {
+                if (league["name"] === "Premier League") {
+                    console.log(
+                        `Retrieved PL league_id: ${league["league_id"]}`
+                    );
+                    return league["league_id"];
+                }
+            });
+        }
+    } catch (e) {
+        console.log(e.message);
+        return;
+    }
+};
+
+getPremierLeagueID(2019);
